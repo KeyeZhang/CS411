2c80ef86a4682b6db2845989e09e5114198cc864
Victor515 <victorlevian@gmail.com>
2017-03-10 16:07:59 -0600

    Finish preference page needing refined: how to resolve when user does not submit any favorite player?

--- preference.php
+++ preference.php
@@ -10,22 +10,21 @@ $player2 = $_POST['fplayer2'];
 $player3 = $_POST['fplayer3'];
 $id = $_SESSION['id'];
 
-$sql_player = "INSERT INTO Favirote_player (Players_Name, Users_ID) 
-VALUES ('$player1', '$id')";
-$sql_player .= "INSERT INTO Favirote_player (Players_Name, Users_ID) 
-VALUES ('$player2', '$id')";
-$sql_player .= "INSERT INTO Favirote_player (Players_Name, Users_ID) 
-VALUES ('$player3', '$id')";
-$result_player = $conn->query($sql_player);
 
-$sql_team = "INSERT INTO Favirote_team (Teams_Name, Users_ID) 
-VALUES ('$team1', '$id')";
-$sql_team = "INSERT INTO Favirote_team (Teams_Name, Users_ID) 
-VALUES ('$team2', '$id')";
-$sql_team = "INSERT INTO Favirote_team (Teams_Name, Users_ID) 
-VALUES ('$team3', '$id')";
-$result_team = $conn->query($sql_team);
+$sql_player = "INSERT INTO Favorite_player (Players_Name, Users_ID) 
+VALUES
+('$player1', '$id'),
+('$player2', '$id'),
+('$player3', '$id');";
+mysqli_query($conn, $sql_player);
 
+
+$sql_team = "INSERT INTO Favorite_team (Teams_Name, Users_ID) 
+VALUES 
+('$team1', '$id'),
+('$team2', '$id'),
+('$team3', '$id');";
+mysqli_query($conn,$sql_team);
 
 header("location: index.html");
 
