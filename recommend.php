b433002b3b1c2237ffad4fec81bd6cc0f76c1576
ruperrrt <sonic931013@gmail.com>
2017-04-25 21:20:42 -0500

    updated recommendation

--- recommend.php
+++ recommend.php
@@ -5,6 +5,13 @@
 
 
 <?php
+    $FTpara = $_POST["FT"];
+    $FPpara = $_POST["FP"];
+    $LOpara = $_POST["LO"];
+    $STpara = $_POST["ST"];
+    $BLpara = $_POST["BL"];
+    $LGpara = $_POST["LG"];
+
 	function favteam($usrid,$home,$road){
 		$sql1 = "SELECT * FROM `Favorite_team` WHERE Users_ID=$usrid AND Teams_Name='$road';";
 		$sql2 = "SELECT *  FROM `Favorite_team` WHERE Users_ID=$usrid AND Teams_Name='$home';";
@@ -31,10 +38,13 @@
 	}
 	
 	function days($date){
-		$sql = "SELECT DATEDIFF (STR_TO_DATE('$date','%d-%M-%Y'),'2017-4-1')";
+		$sql = "SELECT DATEDIFF (STR_TO_DATE('$date','%d-%M-%Y'),CURDATE())";
 		$result =  mysql_query($sql) or die(mysql_error());
 		return $result;
 	}
+	function scale($a,$b){
+        return $a-$b;
+    }
 	mysql_connect("localhost","staysimple_dalao1","Dalaodalao1");
 	mysql_select_db("staysimple_sometimesnaive_my_DB");
 	$usrid = $_SESSION['id'];
@@ -60,16 +70,48 @@
 	$sql3 = "SELECT * FROM `game4`;";
 	$result =  mysql_query($sql3) or die(mysql_error());
 	//test sql3
+
+	//4/24/2017
+	$favteam1s = array();
+	$favplayer1s = array();
+	$location1s = array();
+	$day1s = array();
+	$block1s = array();
+	$steal1s = array();
+	while ($row1 = mysql_fetch_assoc($result)) {
+		$favteam1 = favteam($usrid,$row1['home'],$row1['road']); 
+		$favteam1s[$row1['gid']] = $favteam1;
+		$favplayer1 = favplayer($usrid,$row1['home'],$row1['road']);
+		$favplayer1s[$row1['gid']] = $favplayer1;
+		$location1 = location($usrid,$row1['home'],$row1['road']);
+		$location1s[$row1['gid']] = $location1;
+		$day1 = mysql_result(days($row1['date']), 0);
+		$day1s[$row1['gid']] = $day1;
+		$block1 = teamstats($row1['home'])['BLK']+teamstats($row1['road'])['BLK'];
+		$block1s[$row1['gid']] = $block1;
+		$steal1 = teamstats($row1['home'])['STL']+teamstats($row1['road'])['STL'];
+		$steal1s[$row1['gid']] =$steal1;
+	}
+	//foreach ($favteam1s as $gid => $favteam1) {
+	//	echo $gid." => ".$favteam1."<br>";
+	//}	
+		$maxteam = max($favteam1s);    $minteam = min($favteam1s);
+		$maxplayer = max($favplayer1s);  $minplayer = min($favplayer1s);
+	    $maxlocation = max($location1s);   $minlocation = min($location1s);
+	    $maxday = max($day1s);   $minday = min($day1s);
+	    $maxblock = max($block1s);  $minblock = min($block1s);
+	    $maxsteal = max($steal1s);  $minsteal = min($steal1s);  
+	                        
+
+	$result =  mysql_query($sql3) or die(mysql_error());
 	while ($row = mysql_fetch_assoc($result)) {
-		$score = 0;
-		$score += 6 * favteam($usrid,$row['home'],$row['road']);     //the # of favorite teams included in a certain game
-		$score += 3 * favplayer($usrid,$row['home'],$row['road']);    //the # of favorite players included in a certain game
-		$score += 1.25 * location($usrid,$row['home'],$row['road']);   //the # of team in user's hometown included in a certain game
-		$score += 0.03 * (teamstats($row['home'])['3P_percent']+teamstats($row['road'])['3P_percent']);  //add up the 3 point effect
-		$score += 0.045 * (teamstats($row['home'])['STL']+teamstats($row['road'])['STL']);  //add up the steal effect
-		$score += 0.035 * (teamstats($row['home'])['AST']+teamstats($row['road'])['AST']);  //add up the assist effect
-		$score += 0.06 * (teamstats($row['home'])['BLK']+teamstats($row['road'])['BLK']);  //add up the block effect
-		$score -= 0.05 * mysql_result(days($row['date']), 0);  //later games are less likely to be recommended  
+        $score = 0;
+        $score += $FTpara * favteam($usrid,$row['home'],$row['road'])/scale($maxteam,$minteam);     //the # of favorite teams included in a certain game
+        $score += $FPpara * favplayer($usrid,$row['home'],$row['road'])/scale($maxplayer,$minplayer);    //the # of favorite players included in a certain game
+        $score += $LOpara * location($usrid,$row['home'],$row['road'])/scale($maxlocation,$minlocation);   //the # of team in user's hometown included in a certain game
+        $score += $STpara * (teamstats($row['home'])['STL']+teamstats($row['road'])['STL'])/scale($maxsteal,$minsteal);  //add up the steal effect
+        $score += $BLpara * (teamstats($row['home'])['BLK']+teamstats($row['road'])['BLK'])/scale($maxblock,$minblock);  //add up the block effect
+        $score -= $LGpara * mysql_result(days($row['date']), 0)/scale($maxday,$minday);  //later games are less likely to be recommended
 		//echo mysql_result(days($row['date']), 0)."<br><br>";
 		$scores[$row['gid']] = $score;
 	}
@@ -84,16 +126,24 @@
 	$rec1 = array_slice($scores,0,1,ture);
 	$rec2 = array_slice($scores,1,1,ture);
 	$rec3 = array_slice($scores,2,1,ture);
+	$rec4 = array_slice($scores,3,1,ture);
+	$rec5 = array_slice($scores,4,1,ture);
 	//print_r($rec1);
 	$r1=key($rec1);
 	$r2=key($rec2);
 	$r3=key($rec3);
-	$sql4 = "SELECT * FROM `game4` WHERE gid = '$r1' OR gid = '$r2'OR gid = '$r3';";
+	$r4=key($rec4);
+	$r5=key($rec5);
+	$sql4 = "SELECT * FROM `game4` WHERE gid = '$r1' OR gid = '$r2'OR gid = '$r3'OR gid = '$r4'OR gid = '$r5';";
 	$result =  mysql_query($sql4) or die(mysql_error());	
 	while ($row = mysql_fetch_assoc($result)) {
 		foreach ($row as $key => $value) {
 		echo $key." => ".$value."<br>";
 	}
 	}	
-
+$_SESSION['FT'] = $_POST["FT"];
+$_SESSION['FT'] = $_POST["FT"];
+$_SESSION['FT'] = $_POST["FT"];
+$_SESSION['FT'] = $_POST["FT"];
+$_SESSION['FT'] = $_POST["FT"];
 	?>
